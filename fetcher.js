3e472f12a0196af949b3236438f2394444cd095c
RamzRjs <60762747+RameesaRijas@users.noreply.github.com>
2021-09-23 11:29:11 -0400

    changes

--- fetcher.js
+++ fetcher.js
@@ -4,13 +4,13 @@ const args = process.argv.splice(2);
 
 request(args[0], (error, response, body) => {
   if (error) {
-    console.log(response, error.code);
-    process.exit();
+    console.log("Problem ", error.message);
+    return
   }
   fs.access(args[1], function(error) {
     if (error) {
       console.log("Directory does not exist.")
-      process.exit();
+      return
     } else {
     fs.writeFile(args[1], body, err => {
       if (err) {
